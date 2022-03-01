06efffdac67812afdfbc1c6d6eae50b066aac5bf
felipenevess <felipe012neves@gmail.com>
2022-02-28 23:19:25 -0300

    section podcast base feita

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 
 import SectionSudiovisualMarket from './components/sections/SectionSudiovisualMarket';
+import StartYourPodcast from './components/sections/StartYourPodcast';
 
 function App() {
   return (
     <main>
       <Header />
       <SectionSudiovisualMarket />
+      <StartYourPodcast />
     </main>
   );
 }
