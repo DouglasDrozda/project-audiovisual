616c4e33a411170abb1db6857b66dcf6eb58ad18
DouglasDrozda <lowdouglas9712@gmail.com>
2022-03-03 13:47:38 -0300

    Adicionando imagem de servicos de video

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import SectionSudiovisualMarket from './components/sections/SectionSudiovisualMarket';
 import StartYourPodcast from './components/sections/StartYourPodcast';
 import Schedule from './components/sections/Schedule';
+import VideoService from './components/sections/VideoService';
 import SectionAboutCompany from './components/sections/SectionAboutCompany';
 import SectionIndications from './components/sections/SectionIndications';
 
@@ -13,8 +14,9 @@ function App() {
       <SectionSudiovisualMarket />
       <StartYourPodcast />
       <Schedule />
-      <SectionAboutCompany />
+      <VideoService />
       <SectionIndications />
+      <SectionAboutCompany />
     </main>
   );
 }
