bcab9175f81b7c46ff102dff38d2a5e8a4b5e6b3
DouglasDrozda <lowdouglas9712@gmail.com>
2022-03-02 21:43:27 -0300

    tentando ajustar video

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,15 +47,14 @@ function Header() {
           </div>
         )
       }
+      <video loop muted autoPlay className="video-header">
+        <source src="https://cdn-prod-ccv.adobe.com/-BPyu3eZxyS/rend/-BPyu3eZxyS_576.mp4?hdnts=st%3D1646086968%7Eexp%3D1646346168%7Eacl%3D%2Fshared_assets%2Fimage%2F*%21%2Fz%2F-BPyu3eZxyS%2Frend%2F*%21%2Fi%2F-BPyu3eZxyS%2Frend%2F*%21%2F-BPyu3eZxyS%2Frend%2F*%21%2F-BPyu3eZxyS%2Fimage%2F*%7Ehmac%3D50191da5b188b3de9e5e15e96ebb0cb1cb674471d07d04906e561d8d2dad1ee9" type="video/mp4" />
+        <source src="media/Cover - A Thousand Years.ogg" type="video/ogg" />
+        <source src="media/Cover - A Thousand Years.webm" type="video/webm" />
+        Formato de vídeo não suportado
+      </video>
     </div>
   );
 }
 
 export default Header;
-
-        // <video loop muted autoPlay className="video-header">
-        //   <source src="https://cdn-prod-ccv.adobe.com/-BPyu3eZxyS/rend/-BPyu3eZxyS_576.mp4?hdnts=st%3D1646086968%7Eexp%3D1646346168%7Eacl%3D%2Fshared_assets%2Fimage%2F*%21%2Fz%2F-BPyu3eZxyS%2Frend%2F*%21%2Fi%2F-BPyu3eZxyS%2Frend%2F*%21%2F-BPyu3eZxyS%2Frend%2F*%21%2F-BPyu3eZxyS%2Fimage%2F*%7Ehmac%3D50191da5b188b3de9e5e15e96ebb0cb1cb674471d07d04906e561d8d2dad1ee9" type="video/mp4" />
-        //   <source src="media/Cover - A Thousand Years.ogg" type="video/ogg" />
-        //   <source src="media/Cover - A Thousand Years.webm" type="video/webm" />
-        //   Formato de vídeo não suportado
-        // </video>
